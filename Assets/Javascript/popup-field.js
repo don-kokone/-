ea94fa1b0a95624ff13675703d324fe39a81d25f
Kaloyan Zahariev <49496890+don-kokone@users.noreply.github.com>
2021-05-15 23:56:12 +0300

    Add files via upload

--- Assets/Javascript/popup-field.js
+++ Assets/Javascript/popup-field.js
@@ -1,8 +1,63 @@
+//popup 1
 
-
-function myFunction() {
-    var element = document.getElementsByClassName('c-popup-container')
-    console.log("click")
-    element[0].classList.add("visible")
+function openPopUp() {
+    let open = document.getElementsByClassName('c-popup-container')
+    open[0].classList.add("visible")
   }
 
+  function closePopUp() {
+    let close = document.getElementsByClassName('c-popup-container')
+    close[0].classList.remove("visible")
+}
+//popup 2
+  function openPopUpTwo() {
+    let openTwo = document.getElementsByClassName('c-popup-container two')
+    openTwo[0].classList.add("visible")
+  }
+
+  function closePopUpTwo() {
+    let close = document.getElementsByClassName('c-popup-container two')
+    close[0].classList.remove("visible")
+}
+
+//popup 3
+
+function openPopUpThree() {
+  let openThree = document.getElementsByClassName('c-popup-container three')
+  openThree[0].classList.add("visible")
+}
+
+function closePopUpThree() {
+  let close = document.getElementsByClassName('c-popup-container three')
+  close[0].classList.remove("visible")
+}
+//popup 4
+function openPopUpFour() {
+  let openFour = document.getElementsByClassName('c-popup-container four')
+  openFour[0].classList.add("visible")
+}
+
+function closePopUpFour() {
+  let close = document.getElementsByClassName('c-popup-container four')
+  close[0].classList.remove("visible")
+}
+//popup 5
+function openPopUpFive() {
+  let openFour = document.getElementsByClassName('c-popup-container five')
+  openFour[0].classList.add("visible")
+}
+
+function closePopUpFive() {
+  let close = document.getElementsByClassName('c-popup-container five')
+  close[0].classList.remove("visible")
+}
+//popup 6
+function openPopUpSix() {
+  let openFive = document.getElementsByClassName('c-popup-container six')
+  openFive[0].classList.add("visible")
+}
+
+function closePopUpSix() {
+  let close = document.getElementsByClassName('c-popup-container six')
+  close[0].classList.remove("visible")
+}
