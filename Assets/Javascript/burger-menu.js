73192c561c6e1c78d1b2104d4368a0812ceda612
Kaloyan Zahariev <49496890+don-kokone@users.noreply.github.com>
2021-05-18 20:55:14 +0300

    Minor changes

--- Assets/Javascript/burger-menu.js
+++ Assets/Javascript/burger-menu.js
@@ -2,3 +2,19 @@ function toggleBurgerMenu() {
     var element = document.getElementById("navigation");
     element.classList.toggle("active");
   }
+
+  console.log(element)
+
+// In progress
+  var links = document.getElementsByClassName("link");
+  var element = document.getElementById("navigation");
+
+
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      var element = document.getElementById("navigation")
+      element.classList.remove("active")
+      });
+  });
+  
+ 
